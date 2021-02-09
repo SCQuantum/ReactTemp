578f2376dcd07806dc16c831dc7e211c46113984
SCQuantum <scqdevelopers@gmail.com>
2021-02-09 14:23:21 -0500

    Quick button fix

--- src/Landing.js
+++ src/Landing.js
@@ -9,7 +9,31 @@ function Landing(){
             <div id="sideBar">
                 <div className="button-container">
                     <Button>Test</Button>
-                </div>               
+                </div>       
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>     
+                <div className="button-container">
+                    <Button>Test</Button>
+                </div>             
             </div>
             <div id="themeMenu">
                 <div className="themeDiv">test theme div</div>
@@ -20,27 +44,6 @@ function Landing(){
                 <div className="themeDiv">test theme div</div>
                 <div className="themeDiv">test theme div</div>
                 <div className="themeDiv">test theme div</div>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
-            </div>
-            <div className="button-container">
-                <Button>Test</Button>
             </div>
         </div>
     );
