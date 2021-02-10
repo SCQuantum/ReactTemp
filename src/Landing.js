8f5d5c0250de5fd696f12c84d48f30d397fd730a
SCQuantum <scqdevelopers@gmail.com>
2021-02-10 10:09:26 -0500

    Redesigning Landing Page

--- src/Landing.js
+++ src/Landing.js
@@ -5,35 +5,8 @@ import Button from './components/Button/Button';
 function Landing(){
     
     return(
-        // Full-Page Div for Landing Page
-        <div className="container"> 
-            {/* Side Navbar on Landing Page  */}
-            <div id="sideBar">
-                 {/* Div Containing All buttons Within the Sidebar */}
-                <div className="button-container">
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                    <Button>Test</Button>
-                </div> 
-            </div>
-            {/* Div Containing the Different Theme Options in the Horizontal Sliding Section */}
-            <div id="themeMenu"> 
-                {/* Divs Containing the Class that will Style each Choosable Theme  */}
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-                <div className="themeDiv">test theme div</div>
-            </div>           
+        <div>
+            
         </div>
     );
 }
